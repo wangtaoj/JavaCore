a9f98ab51d6ffd96314e49c216ccf0f61b4ac369
wangtao <wangtao970503@gmail.com>
2019-04-12 14:08:42 +0800

    jvm

--- src/main/java/com/wangtao/jvm/thread/HappenBefore.java
+++ src/main/java/com/wangtao/jvm/thread/HappenBefore.java
@@ -3,25 +3,12 @@ package com.wangtao.jvm.thread;
 /**
  * 测试先行发生原则的volatile变量规则
  * 在不同线程环境下对一个volatile变量的写操作总是先行发生于(后面)对这个变量的读操作
- *
  * @author wangtao
  * Created at 2019/1/6 17:30
  */
 public class HappenBefore {
 
-    public static void main(String[] args) {
-        A a = new A();
-
-        for (int i = 0; i < 1000; i++) {
-            new Thread(()-> a.setValue(5)).start();
-            new Thread(()->System.out.println(a.getValue())).start();
-        }
-    }
-
-}
-
-class A {
-    private volatile int value;
+    private volatile int value = 1;
 
     void setValue(int value) {
         this.value = value;
@@ -29,5 +16,23 @@ class A {
 
     int getValue() {
         return value;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 200; i++) {
+            HappenBefore happenBefore = new HappenBefore();
+            new Thread(() -> happenBefore.setValue(5)).start();
+            new Thread(() -> assertBoolean(happenBefore.getValue() == 5)).start();
+            // 直到上面两个线程运行完毕再运行
+            while (Thread.activeCount() > 2) {
+                Thread.yield();
+            }
+        }
+    }
+
+    private static void assertBoolean(boolean condition) {
+        if (!condition) {
+            throw new AssertionError();
+        }
     }
 }
--- src/main/java/com/wangtao/jvm/thread/VolatileTest.java
+++ src/main/java/com/wangtao/jvm/thread/VolatileTest.java
@@ -1,6 +1,8 @@
 package com.wangtao.jvm.thread;
 
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author wangtao
  * created 2018/2/7
@@ -9,20 +11,35 @@ public class VolatileTest {
 
     private static volatile int race = 0;
 
+    private static final AtomicInteger count = new AtomicInteger(0);
+
     private static final int THREAD_COUNT = 20;
 
+    @SuppressWarnings("NonAtomicOperationOnVolatileField")
     private static void increace() {
         race++;
+        count.incrementAndGet();
     }
 
     public static void main(String[] args) {
-        for(int i = 0; i < THREAD_COUNT; i++) {
-            new Thread(()->{
-                for(int j = 0; j < 10000; j++)
+        /*ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
+        Thread[] threads = new Thread[threadGroup.activeCount()];
+        threadGroup.enumerate(threads);
+        for(Thread thread : threads) {
+            System.out.println(thread.getName());
+        }*/
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 10000; j++) {
                     increace();
+                }
             }).start();
         }
-
-        System.out.println(race);
+        // main线程所在的线程组中最后活动线程会存在2个
+        while (Thread.activeCount() > 2) {
+            Thread.yield();
+        }
+        System.out.println("Primitive: " + race);
+        System.out.println("AtomicInteger: " + count);
     }
 }
