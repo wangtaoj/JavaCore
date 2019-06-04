be1e78459767da624e7dd6285f0c3ed58ea1e052
wangtao <wangtao970503@gmail.com>
2019-06-04 22:06:03 +0800

    FileChannel小例子.

---
+++ src/main/java/com/wangtao/nio/FileChannelTest.java
@@ -0,0 +1,60 @@
+package com.wangtao.nio;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * @author wangtao
+ * Created at 2019/6/4 21:51
+ */
+public class FileChannelTest {
+
+    /**
+     * 从指定文件中读取一个内容
+     */
+    @Test
+    public void read() {
+        try (FileChannel channel = FileChannel.open(Paths.get("README.md"), StandardOpenOption.READ)) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            // 从channel中读取数据写入到buffer中
+            while(channel.read(buffer) > 0) {
+                // 切成读模式
+                buffer.flip();
+                System.out.print(new String(buffer.array(), buffer.position(), buffer.limit()));
+                // 清空, 以便下次写入
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 复制一个文件
+     */
+    @Test
+    public void copy() {
+        // 创建一个FileChannel, 如果指定的文件存在, 则以追加的方式写入
+        // 如果不存在, 先创建, 然后追加
+        try (FileChannel readChannel = FileChannel.open(Paths.get("README.md"), StandardOpenOption.READ);
+             FileChannel writeChannel = FileChannel.open(Paths.get("D:\\copy.md"),
+                     StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
+            // 将自己通道内的内容写入到给定的通道中, 此方法不会改变本通道的位置
+            readChannel.transferTo(0, readChannel.size(), writeChannel);
+            // 因为没有改变位置, 可以继续从该通道中读取数据
+            ByteBuffer buf = ByteBuffer.allocate(1024);
+            while(readChannel.read(buf) > 0) {
+                buf.flip();
+                System.out.print(new String(buf.array(), buf.position(), buf.limit()));
+                buf.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
