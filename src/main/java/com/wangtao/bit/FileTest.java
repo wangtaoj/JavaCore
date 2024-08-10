36087865b2aef06602e4cacd611071782e7e43ca
wangtao <wangtao970503@foxmail.com>
2024-08-10 18:57:13 +0800

    增加junit5

--- src/main/java/com/wangtao/bit/FileTest.java
+++ src/main/java/com/wangtao/bit/FileTest.java
@@ -1,7 +1,7 @@
 package com.wangtao.bit;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author wangtao
@@ -12,23 +12,24 @@ public class FileTest {
     @Test
     public void testPermission() {
         File file = new File(File.R | File.W | File.X);
-        Assert.assertTrue(file.hasPermission(File.R));
+        Assertions.assertTrue(file.hasPermission(File.R));
         // 判断是否同时拥有R和W权限
-        Assert.assertTrue(file.hasPermission(File.R | File.W));
+        Assertions.assertTrue(file.hasPermission(File.R | File.W));
 
         // 去掉X权限
         file.removePermission(File.X);
-        Assert.assertTrue(file.hasPermission(File.R | File.W));
-        Assert.assertFalse(file.hasPermission(File.X));
+        Assertions.assertTrue(file.hasPermission(File.R | File.W));
+        Assertions.assertFalse(file.hasPermission(File.X));
 
         // 加上X权限
         file.addPermission(File.X);
-        Assert.assertTrue(file.hasPermission(File.X));
+        Assertions.assertTrue(file.hasPermission(File.X));
 
         int oldPermission = file.getPermission();
         // 添加已有的权限
         file.addPermission(File.X);
         // 没有任何变化
-        Assert.assertEquals(oldPermission, file.getPermission());
+        Assertions.assertEquals(oldPermission, file.getPermission());
+        Assertions.assertThrows(IllegalArgumentException.class, () -> file.setPermission(16));
     }
 }
